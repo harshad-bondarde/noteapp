b4f7e7764ad75e1678af68445d0457ed10eff09e
Harshad <harshadbondarde2206@gmail.com>
2024-10-13 00:16:25 +0530

    initialised backend and frontned

---
+++ backend/index.js
@@ -0,0 +1,15 @@
+const express=require('express')
+const dotenv=require('dotenv')
+
+const app=express()
+
+dotenv.config()
+const PORT=process.env.PORT || 3000
+
+app.get("/",(req,res)=>{
+    res.send("API is running...")
+})
+
+app.listen(PORT,()=>{
+    console.log(`server is running at port ${PORT}`)
+})
